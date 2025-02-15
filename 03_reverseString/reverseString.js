7a0135a8db7f22ac3a3a3db3156cba13abb690f4
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-02-14 18:56:48 -0800

    Solve problem 3 Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    for (let i = string.length-1; i >= 0; i--) {
+        let character = string[i];
+        newString += character;
+    }
+    return newString;
 };
 
 // Do not edit below this line
